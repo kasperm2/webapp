dfe6eadb76f45675978d626145100f663703f052
Kasper <eaakamik@students.eaaa.dk>
2019-09-12 09:13:11 +0200

    SPA opsat

---
+++ js/main.js
@@ -0,0 +1,106 @@
+"use strict";
+
+// hide all pages
+function hideAllPages() {
+  let pages = document.querySelectorAll(".page");
+  for (let page of pages) {
+    page.style.display = "none";
+  }
+}
+
+// show page or tab
+function showPage(pageId) {
+  hideAllPages();
+  document.querySelector(`#${pageId}`).style.display = "block";
+  location.href = `#${pageId}`;
+  setActiveTab(pageId);
+}
+
+// sets active tabbar/ menu item
+function setActiveTab(pageId) {
+  let pages = document.querySelectorAll(".tabbar a");
+  for (let page of pages) {
+    if (`#${pageId}` === page.getAttribute("href")) {
+      page.classList.add("active");
+    } else {
+      page.classList.remove("active");
+    }
+
+  }
+}
+
+// set default page
+function setDefaultPage() {
+  let page = "home";
+  if (location.hash) {
+    page = location.hash.slice(1);
+  }
+  showPage(page);
+}
+
+
+function showLoader(show) {
+  let loader = document.querySelector('#loader');
+  if (show) {
+    loader.classList.remove("hide");
+  } else {
+    loader.classList.add("hide");
+  }
+}
+
+// ========== Firebase sign in functionality ========== //
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD12x3NRaz7apxsiO4Aaro56Iqt0tAJcjg",
+  authDomain: "web-applikation-2d72a.firebaseapp.com",
+  databaseURL: "https://web-applikation-2d72a.firebaseio.com",
+  projectId: "web-applikation-2d72a",
+  storageBucket: "web-applikation-2d72a.appspot.com",
+  messagingSenderId: "618467941437",
+  appId: "1:618467941437:web:55e26fce9ee0c50ce75e54"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firebase UI configuration
+const uiConfig = {
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID
+  ],
+  signInSuccessUrl: '#home',
+};
+
+// Init Firebase UI Authentication
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// Listen on authentication state change
+firebase.auth().onAuthStateChanged(function(user) {
+  let tabbar = document.querySelector('#tabbar');
+  console.log(user);
+  if (user) { // if user exists and is authenticated
+    setDefaultPage();
+    tabbar.classList.remove("hide");
+    appendUserData(user);
+  } else { // if user is not logged in
+    showPage("login");
+    tabbar.classList.add("hide");
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+  showLoader(false);
+});
+
+// sign out user
+function logout() {
+  firebase.auth().signOut();
+}
+
+function appendUserData(user) {
+  document.querySelector('#profile').innerHTML += `
+    <h3>${user.displayName}</h3>
+    <p>${user.email}</p>
+  `;
+}
